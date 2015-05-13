350d8ad82c46ea99a49938e571cf19bd265820f4
Luis Antonio Sánchez <luis.sanchez.franco@gmail.com>
2015-05-12 19:08:33 -0500

    Primer commit de front end con estructura básica

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,3 @@
+
+var momentos = angular.module('momentos', ['ngResource']);
+
---
+++ app/assets/javascripts/controllers/RootController.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Owen on 12/05/15.
+ */
+
+'use strict';
+
+momentos.controller('RootController', ["$scope", "$rootScope", function($scope, $rootScope){
+
+    var initController = function(){
+        console.log('ENTRE AL ROOT CONTROLLER')
+    };
+
+    initController;
+
+}]);
